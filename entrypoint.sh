d27bb988efecdb343e3a2767e3a1d5bea5d14c77
Maximilian Wisgickl <wisgicklma@protonmail.com>
2021-01-14 12:12:41 +0100

    Revert changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,52 @@
-#!/bin/sh -ex
+#!/bin/sh -l
 
-# Inspired from
-#https://github.com/johno/actions-push-subdirectories/blob/master/entrypoint.sh#L10
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
 
-SOURCE_FOLDER="$1"
-DESTINATION_FOLDER="$2"
-GITHUB_USERNAME="$3"
-GITHUB_REPO="$4"
-GITHUB_USER_EMAIL="$5"
+echo "Starts"
+SOURCE_DIRECTORY="$1"
+DESTINATION_GITHUB_USERNAME="$2"
+DESTINATION_REPOSITORY_NAME="$3"
+USER_EMAIL="$4"
+DESTINATION_REPOSITORY_USERNAME="$5"
+TARGET_BRANCH="$6"
+COMMIT_MESSAGE="$7"
 
-CLONE_DIR=output_clone
+if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
+then
+  DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
+fi
 
-apt-get update && apt-get install git
-apk add --no-cache git
+CLONE_DIR=$(mktemp -d)
 
-git config --global user.email "$GITHUB_USER_EMAIL"
-git config --global user.name "$GITHUB_USERNAME"
+echo "Cloning destination git repository"
+# Setup git
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$DESTINATION_GITHUB_USERNAME"
+git clone --single-branch --branch "$TARGET_BRANCH" "git@github.com:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
+ls -la "$CLONE_DIR"
 
-echo "Clone $GITHUB_REPO"
-git clone "git@github.com:$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+TARGET_DIR=$(mktemp -d)
+mv "$CLONE_DIR/.git" "$TARGET_DIR"
 
-echo "Delete old files to handle deletions"
-rm -rf "${CLONE_DIR:?}/$DESTINATION_FOLDER/*"
+echo "Copying contents to git repo"
+cp -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR"
+cd "$TARGET_DIR"
 
-echo "Copy new files"
-cp -r "$SOURCE_FOLDER"/* "$CLONE_DIR"/"$DESTINATION_FOLDER"/
+echo "Files that will be pushed"
+ls -la
 
-cd $CLONE_DIR
+echo "Adding git commit"
+
+ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+
 git add .
-git commit --message "Update from $GITHUB_REPOSITORY"
-git push origin master
+git status
 
-echo "Done!"
+# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+
+echo "Pushing git commit"
+# --set-upstream: sets de branch when pushing to a branch that does not exist
+git push origin --set-upstream "$TARGET_BRANCH"
